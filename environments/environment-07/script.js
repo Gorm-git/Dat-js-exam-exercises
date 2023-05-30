d90a2b8e9aa960f98054e524f89202fffa13b19c
Gorm-git <tneermark@gmail.com>
2023-05-30 12:47:16 +0200

    Update script.js

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,4 +1,5 @@
 "use strict";
+<<<<<<< Updated upstream
 let students = [];
 
 window.addEventListener("load", initApp);
@@ -60,4 +61,80 @@ function isEmailValid(email) {
         return false;
     }
     // return prefix.length >= 4 && domain === "stud.kea.dk";
+=======
+
+/*
+
+Environment: `environment-03`
+
+1. Lav en liste med tre `product`-objekter. Hvert objekt har følgende properties: `name` (string), `price` (number) og `inStock` (boolean).
+2. Lav en funktion der viser en sorteret liste af alle produkter. Produkter som er i `stock` (hvor `inStock` er `true`) skal vises først.
+3. Lav en funktion der ved hjælp af formularen, opretter et nyt `product`-objekt og tilføjer det til listen. Listen på websiden opdateres hver gang, der oprettes et nyt objekt.
+
+*/
+
+const students = [];
+
+window.addEventListener("load", start);
+
+function start() {
+  document
+    .querySelector("#create-student-form")
+    .addEventListener("submit", formClicked);
+  console.log("app is running!");
+}
+
+function createStudent(name, email, age) {
+  const newstudent = {
+    name: name,
+    email: email,
+    age: age,
+  };
+  students.push(newstudent);
+  showStudents();
+}
+
+function formClicked(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = form.age.value;
+
+  createStudent(name, email, age);
+}
+
+function showStudents() {
+  document.querySelector("#create-student-form").innerHTML = "";
+  for (let student of students) {
+    if (student.age >= 18) {
+      showStudent(student);
+      console.log("Student is above 18");
+      return true;
+    } else {
+      console.log("Student is not 18");
+      return false;
+    }
+  }
+}
+
+function showStudent(student) {
+  const html =
+    /*html*/
+    `
+      <tr>
+        <td>${student.name} </td>
+
+        <td>${student.email} </td>
+
+        <td>${student.age} </td>
+      </tr>
+      `;
+
+  document
+    .querySelector("#students-table-body")
+    .insertAdjacentHTML("beforeend", html);
+>>>>>>> Stashed changes
 }
