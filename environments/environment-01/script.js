7524502d6f22a4f74c50f24a9411f11008fd4e3b
Gorm-git <tneermark@gmail.com>
2023-06-09 11:41:03 +0200

    Yup

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,115 @@
 "use strict";
 
+// // # Øvelse 1
+
+// // Environment: `environment-01`
+
+// // 1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en variabel.
+// // 2. Lav en funktion der viser listen på websiden - hver user skal vises med navn og hvorvidt de er aktive eller ej.
+// // 3. Filtrér listen så den kun viser admin-brugere.
+
+// window.addEventListener("load", initapp);
+
+// async function initapp() {
+//   const userList = await getJson();
+//   showUsers(userList);
+//   console.log(userList);
+// }
+
+// async function getJson() {
+//   const user = await fetch("users.json");
+//   const data = await user.json();
+
+//   return data;
+// }
+
+// function showUsers(userList) {
+//   document.querySelector("#userlist").innerHTML = "";
+//   for (const user of userList) {
+//     showUser(user);
+//   }
+// }
+
+// function showUser(userData) {
+//   if (userData.role == "admin") {
+//     const html = /*html*/ `
+//   <article>
+//   <br>
+//   <li>Navn: ${userData.name}</li>
+//   <li>Aktiv: ${userData.active}</li>
+//   <li>Rolle: ${userData.role}</li>
+//   <br>
+//   </article>
+//   `;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// # Øvelse 2
+
+// Environment: `environment-01`
+
+// 1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en variabel.
+// 2. Lav en funktion der viser listen på websiden.
+// 3. Lav en funktion der tæller hvor mange brugere der har rollerne `admin`, `user` og `guest` - og viser antallene på websiden.
+
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  console.log("Java is alive");
+  const userList = await fetchJson();
+  fetchJson(userList);
+  console.log(userList);
+  showUsers(userList);
+  countUsers(userList);
+}
+
+async function fetchJson() {
+  const user = await fetch("users.json");
+  const data = await user.json();
+
+  return data;
+}
+
+function showUsers(userList) {
+  document.querySelector("#userlist").innerHTML = "";
+  for (const user of userList) {
+    showUser(user);
+  }
+}
+
+function showUser(userData) {
+  const html = /*html*/ `
+  <br>
+  <li>Navn: ${userData.name}</li>
+  <li>Aktiv: ${userData.active}</li>
+  <li>Rolle: ${userData.role}</li>
+  <br>
+  
+  `;
+
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
+
+function countUsers(userList) {
+  let admin = 0;
+  let user = 0;
+  let guest = 0;
+
+  for (const count of userList) {
+    if (count.role == "admin") {
+      admin++;
+    }
+    if (count.role == "user") {
+      user++;
+    } else {
+      guest++;
+    }
+  }
+  console.log(admin);
+  console.log(user);
+  console.log(guest);
+  document.getElementById("admin-count").textContent = admin;
+  document.getElementById("user-count").textContent = user;
+  document.getElementById("guest-count").textContent = guest;
+}
--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,61 +1,109 @@
 "use strict";
 
+// window.addEventListener("load", initApp);
+
+// const animals = [];
+
+// function initApp() {
+//     createAnimal("Bo", "abe", 22); // test af del 1
+//     console.log(animals); // test af del 1
+//     // event på formular
+//     document.querySelector("#create-form").addEventListener("submit", createFormSubmitted);
+// }
+
+// function createAnimal(name, type, age) {
+//     // nyt animal objekt med værdier fra argumenter
+//     const animal = {
+//         name: name,
+//         type: type,
+//         age: age
+//     };
+//     animals.push(animal); // det nye objekt tilføjes den globale liste, animals
+//     return animal;
+// }
+
+// function createFormSubmitted(event) {
+//     event.preventDefault(); // prevent default submit event
+
+//     const form = event.target; // reference til formularen
+//     // få fat i værdier fra formular
+//     const name = form.name.value;
+//     const type = form.type.value;
+//     const age = form.age.value;
+//     // kald createAnimal med værdier fra formular
+//     createAnimal(name, type, age);
+//     console.log(animals); // test animals
+//     // sørg for at alle animals vises (+ det nye) ved at kalde showAnimals
+//     showAnimals();
+// }
+
+// function showAnimals() {
+//     // sort animals by name
+//     animals.sort(compareName);
+//     console.log(animals);
+//     // reset tbody
+//     document.querySelector("#list-container tbody").innerHTML = "";
+
+//     for (const animal of animals) {
+//         const html = /*html*/ `
+//             <tr>
+//                 <td>${animal.name}</td>
+//                 <td>${animal.type}</td>
+//                 <td>${animal.age}</td>
+//             </tr>
+//         `;
+//         document.querySelector("#list-container tbody").insertAdjacentHTML("beforeend", html);
+//     }
+// }
+
+// function compareName(a, b) {
+//     return a.name.localeCompare(b.name);
+// }
+
+// # Øvelse 3
+
+// Environment: `environment-02`
+
+// 1. Lav en funktion der modtager `name`, `type` og `age`,
+// opretter et `animal` objekt med de tre properties,
+// og tilføjer det til en global liste, `animals`.
+// 2. Lav en funktion der reagerer når brugeren trykker på
+// "Create new" knappen, og opretter et nyt `animal` objekt med
+// de data der er tastet ind på websiden. Test at objektet er
+// tilføjet den globale liste, `animals`.
+// 3. Lav en funktion der viser listen af
+// alle animal-objekter - sorteret alfabetisk.
+// Listen opdateres hver gang brugeren opretter et nyt.
+
 window.addEventListener("load", initApp);
 
 const animals = [];
 
 function initApp() {
-    createAnimal("Bo", "abe", 22); // test af del 1
-    console.log(animals); // test af del 1
-    // event på formular
-    document.querySelector("#create-form").addEventListener("submit", createFormSubmitted);
+  console.log("42");
+  createAnimal("Bo", "abe", 22);
+  document
+    .querySelector("#create-form")
+    .addEventListener("submit", createSubmitAnimal);
 }
 
 function createAnimal(name, type, age) {
-    // nyt animal objekt med værdier fra argumenter
-    const animal = {
-        name: name,
-        type: type,
-        age: age
-    };
-    animals.push(animal); // det nye objekt tilføjes den globale liste, animals
-    return animal;
+  const newAnimal = {
+    name: name,
+    type: type,
+    age: age,
+  };
+  animals.push(newAnimal);
+  console.log(animals);
 }
 
-function createFormSubmitted(event) {
-    event.preventDefault(); // prevent default submit event
+function createSubmitAnimal(event) {
+  event.preventDefault();
 
-    const form = event.target; // reference til formularen
-    // få fat i værdier fra formular
-    const name = form.name.value;
-    const type = form.type.value;
-    const age = form.age.value;
-    // kald createAnimal med værdier fra formular
-    createAnimal(name, type, age);
-    console.log(animals); // test animals
-    // sørg for at alle animals vises (+ det nye) ved at kalde showAnimals
-    showAnimals();
-}
+  const form = event.target;
 
-function showAnimals() {
-    // sort animals by name
-    animals.sort(compareName);
-    console.log(animals);
-    // reset tbody
-    document.querySelector("#list-container tbody").innerHTML = "";
-
-    for (const animal of animals) {
-        const html = /*html*/ `
-            <tr>
-                <td>${animal.name}</td>
-                <td>${animal.type}</td>
-                <td>${animal.age}</td>
-            </tr>
-        `;
-        document.querySelector("#list-container tbody").insertAdjacentHTML("beforeend", html);
-    }
-}
-
-function compareName(a, b) {
-    return a.name.localeCompare(b.name);
+  const name = form.name.value;
+  const type = form.type.value;
+  const age = Number(form.age.value);
+  createAnimal(name, type, age);
 }
