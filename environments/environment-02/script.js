46f769058a28c5db921d25f12638e4f679cc14d0
Gorm-git <tneermark@gmail.com>
2023-06-09 15:41:47 +0200

    Exercise 5

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -75,35 +75,100 @@
 // alle animal-objekter - sorteret alfabetisk.
 // Listen opdateres hver gang brugeren opretter et nyt.
 
+// window.addEventListener("load", initApp);
+
+// const animals = [];
+
+// function initApp() {
+//   console.log("42");
+//   // createAnimal("Bo", "abe", 22);
+//   document
+//     .querySelector("#create-form")
+//     .addEventListener("submit", createSubmitAnimal);
+// }
+
+// function createAnimal(name, type, age) {
+//   const newAnimal = {
+//     name: name,
+//     type: type,
+//     age: age,
+//   };
+//   animals.push(newAnimal);
+//   console.log(animals);
+// }
+
+// function createSubmitAnimal(event) {
+//   event.preventDefault();
+
+//   const form = event.target;
+
+//   const name = form.name.value;
+//   const type = form.type.value;
+//   const age = Number(form.age.value);
+//   createAnimal(name, type, age);
+// }
+
+// # Øvelse 5
+
+// Environment: `environment-02`
+
+// 1. Lav en liste med tre `animal` objekter.
+// Hvert objekt har følgende properties: `name`, `type` og `age`.
+// 2. Lav en funktion der viser listen af alle `animal`-objekter
+// - sorteret efter `age`.
+// 3. Lav en funktion der ved hjælp af formularen,
+//  opretter et nyt `animal`-objekt og tilføjer det til den liste.
+//  Listen på websiden opdateres hver gang, der oprettes et nyt objekt.
+
 window.addEventListener("load", initApp);
 
-const animals = [];
+const animals = [
+  { name: "Jacob", type: "Dog", age: 8 },
+  { name: "Thomas", type: "Horse", age: 12 },
+  { name: "Karen", type: "Cat", age: 6 },
+];
 
 function initApp() {
   console.log("42");
-  // createAnimal("Bo", "abe", 22);
-  document
-    .querySelector("#create-form")
-    .addEventListener("submit", createSubmitAnimal);
-}
-
-function createAnimal(name, type, age) {
-  const newAnimal = {
-    name: name,
-    type: type,
-    age: age,
-  };
-  animals.push(newAnimal);
   console.log(animals);
+
+  document.querySelector("form").addEventListener("submit", createAnimal);
+  sortAnimalsAge();
+  showAnimals();
 }
 
-function createSubmitAnimal(event) {
+function showAnimals() {
+  document.querySelector("#list-container tbody").innerHTML = "";
+  for (const animal of animals) {
+    const html = /*html*/ `
+      <tr>
+            <td>${animal.name}</td>
+            <td>${animal.type}</td>
+            <td>${animal.age}</td>
+      </tr>
+  `;
+    document
+      .querySelector("#list-container tbody")
+      .insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function sortAnimalsAge() {
+  return animals.sort((a, b) => a.age - b.age);
+}
+
+function createAnimal(event) {
   event.preventDefault();
 
   const form = event.target;
 
   const name = form.name.value;
   const type = form.type.value;
-  const age = Number(form.age.value);
-  createAnimal(name, type, age);
+  const age = form.age.value;
+
+  const newAnimal = { name, type, age };
+
+  animals.push(newAnimal);
+  sortAnimalsAge();
+  showAnimals(newAnimal);
 }
